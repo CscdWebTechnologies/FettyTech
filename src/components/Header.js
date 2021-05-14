85dd02ed73815331287e88fd7dabf3702bcd4b49
Evans Narh <rapcha@Evanss-MacBook-Pro.local>
2021-05-14 12:18:16 +0000

    navigation bar

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom';
+import './header.css';
+import logo from './assets/images/Consumer_Wordmark.svg';
+
+const Header = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                {/* /* coinbase logo */}
+                <div className="logo">
+                    <img src={logo} alt="coinbase-logo" />
+                </div>
+
+                {/* navigation links */}
+                <ul className="mid-links">
+                    <li>Price</li>
+                    <li>Learn</li>
+                    <li>Individuals</li>
+                    <li>Businesses</li>
+                    <li>Developers</li>
+                    <li>Company</li>
+                </ul>
+
+                {/* user portal links */}
+                <ul className="user-inks">
+                    <li className="sign-in">sign in</li>
+                    <li><button className="get-started">Get started</button></li>
+                </ul>
+            </div>
+        </nav>
+    );       
+}
+
+export default Header;
