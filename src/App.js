66cd1b9608fee86221d97a4db27dd2cf34f4af71
Bright Asimeng <rickyasimeng123@gmail.com>
2021-05-14 19:32:48 -0800

    worked on the space

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
+import Space from "./components/Space";
 import Table from "./components/Table";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <Portfolio />
+      <Space/>
       <Table />
     </div>
   );
